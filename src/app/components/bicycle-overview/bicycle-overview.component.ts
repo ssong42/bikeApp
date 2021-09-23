603c932bcb037e211fa58f5429c32e26fd0ef8a7
Steven Song <stsong42@gmail.com>
2021-09-23 09:11:56 -0700

    [@ssong] created bicycle overview page

--- src/app/components/bicycle-overview/bicycle-overview.component.ts
+++ src/app/components/bicycle-overview/bicycle-overview.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Bicycle } from 'src/app/mock-bicycles';
+import { Part } from 'src/app/mock-parts';
+import { BicyclesService } from 'src/app/services/bicycles.service';
+import { PartsService } from 'src/app/services/parts.service';
 
 @Component({
   selector: 'app-bicycle-overview',
   templateUrl: './bicycle-overview.component.html',
-  styleUrls: ['./bicycle-overview.component.css']
+  styleUrls: ['./bicycle-overview.component.css'],
 })
 export class BicycleOverviewComponent implements OnInit {
+  parts: Part[] = [];
+  bikeParts: Part[] = [];
+  bicycle: any;
+  bicycles: Bicycle[] = [];
+  bicycleId: number = 1;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private bicyclesService: BicyclesService,
+    private partService: PartsService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.route.params.subscribe((params) => {
+      this.bicycleId = params['id'];
+    });
 
+    this.bicyclesService.bicycles$.subscribe((data: Bicycle[]) => {
+      //find the bicycle from bicycles list
+      this.bicycle = data.find(
+        (bike: Bicycle) => bike.id == this.bicycleId
+      );
+    });
+
+    this.partService.parts$.subscribe((data: Part[]) => {
+      this.parts = data;
+
+      // find the parts for the bicycle
+      this.bikeParts = this.parts.filter((p) => {
+        if (this.bicycle.parts.find((pp: Part) => pp.id == p.id)) return true;
+        return false;
+      });
+    });
+  }
 }
--- src/app/components/part/part.component.ts
+++ src/app/components/part/part.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
-import { parts } from 'src/app/mock-parts';
+import { Bicycle } from 'src/app/mock-bicycles';
+import { Part, parts } from 'src/app/mock-parts';
+import { PartsService } from 'src/app/services/parts.service';
 import { ModalComponent} from '../modal/modal.component'
 
 @Component({
@@ -10,16 +12,22 @@ import { ModalComponent} from '../modal/modal.component'
   styleUrls: ['./part.component.css'],
 })
 export class PartComponent implements OnInit {
-  part: any;
+  parts: Part[] = [];
+  part: any = {};
   partId = 1;
 
-  constructor(private route: ActivatedRoute, public dialog: MatDialog) {}
+  constructor(private route: ActivatedRoute, public dialog: MatDialog, private partService: PartsService) {  }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.partId = params['id'];
     });
     //find the part from parts list
+    this.partService.parts$.subscribe(
+      (data: Part[]) => {
+        this.parts = data;
+      }
+    );
     this.part = parts.find((part) => part.id == this.partId);
   }
 
